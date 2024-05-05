4990540cf575eb7310d1c89f55cc6fa2043d55df
AhmedAhmedEG <ahmed.ahmed.a.m.aa12@gmail.com>
2024-05-05 19:14:19 +0300

    Add files via upload

--- RPGMakerDecrypter.Decrypter/RGSSAD.cs
+++ RPGMakerDecrypter.Decrypter/RGSSAD.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -73,7 +73,7 @@ namespace RPGMakerDecrypter.Decrypter
         {
             foreach (ArchivedFile archivedFile in ArchivedFiles)
             {
-                ExtractFile(archivedFile, outputDirectoryPath, overrideExisting: overrideExisting);
+                ExtractFile(archivedFile, outputDirectoryPath, overrideExisting);
             }
         }
 
