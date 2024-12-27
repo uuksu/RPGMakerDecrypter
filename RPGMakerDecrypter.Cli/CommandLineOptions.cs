41909e23865fa962b66ca4ee4b988938c58d3035
Mikko Uuksulainen <mikko.uuksulainen@uuksu.fi>
2024-12-27 18:40:52 +0200

    Improve parameters

--- RPGMakerDecrypter.Cli/CommandLineOptions.cs
+++ RPGMakerDecrypter.Cli/CommandLineOptions.cs
@@ -4,17 +4,16 @@ namespace RPGMakerDecrypter.Cli
 {
     public class CommandLineOptions
     {
-        [Value(0, Required = true, HelpText = "Path to the RGSSAD file.")]
+        [Value(0, Required = true, HelpText = "Path to the .rgssad, .rgss2a or .rgss3a file or to MV and MZ directory.")]
         public string InputPath { get; set; }
 
-        [Option('o', "output", Required = false, HelpText = "Optional output directory path.")]
+        [Option('o', "output", Required = false, HelpText = "Optional output directory path. Required if --reconstruct-project is set to true and input path is a MV or MZ directory.")]
         public string OutputDirectoryPath { get; set; }
 
         [Option('p', "reconstruct-project", Required = false, HelpText = "If set to true, tries to reconstruct the original project.")]
         public bool ReconstructProject { get; set; }
         
-        
-        [Option('w', "overwrite", Required = false, HelpText = "If set to true then it will overwrite files.")]
+        [Option('w', "overwrite", Required = false, HelpText = "If set to true, destination files are overwritten.")]
         public bool Overwrite { get; set; }
     }
 }
